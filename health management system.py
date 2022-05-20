8972e59d4e605eec2cae5c86b1529e797d19470f
Sparsh <29sp96@gmail.com>
2022-05-20 15:23:13 +0530

    removed comments

--- health management system.py
+++ health management system.py
@@ -1,10 +1,4 @@
-# harry rohan sparsh
-# 3 files diet
-# 3 files exercise
-# what you want to do log or retrieve
-# write a fun that takes as input client name=asks which client=asks what you want to log exercise or diet
-# write a fun to retrieve the info of exercise and diet
-# print [timestamp] roti
+
 def getdate():
     import datetime
     return datetime.datetime.now()
